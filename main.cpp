0c6494a77973d50ecfd46bc5c793f12acc8444f4
Carlos Alves <cbc.alves@gmail.com>
2021-07-18 12:29:09 -0300

    Add PlayerBuilder

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <QMetaMethod>
 #include <QMetaProperty>
 
-#include "players/player.h"
+#include "players/playerbuilder.h"
 #include "editorqgadget.h"
 
 int main(int argc, char *argv[])
@@ -14,15 +14,7 @@ int main(int argc, char *argv[])
     qRegisterMetaType<player::Player>("player::Player");
     qRegisterMetaType<player::P2D*>("player::P2D*");
 
-    player::Player player;
-
-    player.m_playerType = player::PlayerType::TANK;
-    player.m_speed = 25.3;
-    player.m_ammunition = 2;
-    player.m_active = true;
-    player.m_numberOfCoordinates = 2;
-    player.m_coordinates = nullptr;
-    player.m_coordinates = static_cast<player::P2D*>(std::realloc(player.m_coordinates, sizeof(player::P2D) * player.m_numberOfCoordinates));
+    player::Player player = player::PlayerBuilder::build(player::PlayerType::TANK, 25.3, 2, true, 2);
 
     player.m_coordinates[1].m_x = 2.2;
     player.m_coordinates[1].m_y = 3.1;
